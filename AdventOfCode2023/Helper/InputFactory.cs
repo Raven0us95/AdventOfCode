9971d485150c51fbeef20ff43d68064c436b6c16
Christian Schäfer <christian.schaefer@ximaj.com>
2023-12-27 21:38:33 +0100

    Add project files.

---
+++ AdventOfCode2023/Helper/InputFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.helper
+{
+    public class InputFactory
+    {
+        private string path = $@"{AppDomain.CurrentDomain.BaseDirectory}";
+        private static InputFactory instance = new InputFactory();
+
+        static InputFactory()
+        {
+
+        }
+        private InputFactory()
+        {
+
+        }
+
+        public static InputFactory Instance => instance;
+
+        public string[] CreateInputArray(string input)
+        {
+            // for each line ending add a string to the array
+            string[] lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            return lines;
+        }
+
+        public string GetInputString(string path)
+        {
+            string output = "";
+            Stream stream = File.OpenRead(path);
+            using (stream)
+            {
+                try
+                {
+                    var input = new StreamReader(stream).ReadToEnd();
+                    output = input;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode2023._1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puzzle1 puzzle1 = new Puzzle1();
+            puzzle1.Solve();
+            Console.ReadLine();
+        }
+    }
+}
