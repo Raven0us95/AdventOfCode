ada24d1b6ab47962dc4d374a9e48968429c30608
Christian Schäfer <christian.schaefer@ximaj.com>
2024-02-23 15:09:04 +0100

    add CamelCards

---
+++ AdventOfCode2023/Puzzles/day7/CamelCards.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode2023.models.abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Puzzles.day7
+{
+    public class CamelCards : PuzzleBase
+    {
+        public CamelCards(string input) : base(input)
+        {
+        }
+
+        public override void Solve()
+        {
+            var input = GetInputStringArray();
+        }
+
+        protected override string GetDefaultInputFromDerived()
+        {
+            return "";
+        }
+    }
+}
