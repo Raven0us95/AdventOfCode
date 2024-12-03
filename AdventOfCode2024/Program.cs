e276d4b35fe618be27932a1957346dd86beeaf10
Christian Schäfer <christian.schaefer@ximaj.com>
2024-12-03 13:00:29 +0100

    add Message if default case is hit

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -38,7 +38,7 @@ internal class Program
             {
                 case 1: new Locations(input, isPart2); break;
                 
-                default:
+                default: Console.WriteLine("this day is not yet implemented");
                     break;
             }
 
