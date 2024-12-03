c8a4429896dc7c56e99ecc1e8f09b17feb0e78a4
Christian Schäfer <christian.schaefer@ximaj.com>
2024-12-03 17:23:39 +0100

    day 3

--- AdventOfCode2024/Puzzles/day3/CorruptedData.cs
+++ AdventOfCode2024/Puzzles/day3/CorruptedData.cs
@@ -10,23 +10,64 @@ namespace AdventOfCode2024.Puzzles.day3
 {
     public class CorruptedData : PuzzleBase
     {
-        public CorruptedData( string input, bool isPart2) : base(input, isPart2)
+        public CorruptedData(string input, bool isPart2) : base(input, isPart2)
         {
         }
         public override void SolvePart1()
         {
             Regex regex = new Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)");
-            var matches = regex.Matches(Input);
+            var matches = regex.Matches(Input).Cast<Match>().ToList();
+
+            var sum = 0;
+            foreach (var match in matches)
+            {
+                int firstValue = int.Parse(match.Groups[1].Value);
+                int secondValue = int.Parse(match.Groups[2].Value);
+                sum += firstValue * secondValue;
+            }
+            Console.WriteLine($"solution is {sum}");
         }
 
         public override void SolvePart2()
         {
-            throw new NotImplementedException();
+            //Input = GetDefaultInputFromDerived();
+            Regex mulRegex = new Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+            var matches = mulRegex.Matches(Input).Cast<Match>().ToList();
+            Regex doRegex = new Regex("do\\(\\)");
+            var doMatches = doRegex.Matches(Input).Cast<Match>().ToList();
+            Regex dontRegex = new Regex("don't\\(\\)");
+            var dontMatches = dontRegex.Matches(Input).Cast<Match>().ToList();
+
+            var sum = 0;
+            bool isEnabled = true;
+            var nextIndex = 0;
+
+            var doIndicies = doMatches.Select(x => x.Index).ToList();
+            var dontIndicies = dontMatches.Select(x => x.Index).ToList();
+            var mergedIndicies = doIndicies.Concat(dontIndicies).OrderBy(x => x).ToArray();
+
+            foreach (var match in matches)
+            {
+                if (nextIndex < mergedIndicies.Length && match.Index > mergedIndicies[nextIndex])
+                {
+                    isEnabled = doIndicies.Contains(mergedIndicies[nextIndex]);
+                    nextIndex++;
+                }
+
+                if (isEnabled)
+                {
+                    int firstValue = int.Parse(match.Groups[1].Value);
+                    int secondValue = int.Parse(match.Groups[2].Value);
+                    sum += firstValue * secondValue;
+                }
+            }
+
+            Console.WriteLine($"Solution is {sum}");
         }
 
         protected override string GetDefaultInputFromDerived()
         {
-            return "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+            return "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         }
     }
 }
